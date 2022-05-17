f7bf35733faefa98b72e115b5a80af8f68f9e2f3
ShayFae <ifrahnoor108@gmail.com>
2022-05-17 15:26:41 -0400

    Add states for weather info

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,18 +1,50 @@
+import React, { useState, useEffect } from 'react'
 import './Weather.css'
+import axios from 'axios'
 
 export default function Weather() {
-  const weather = `https://api.openweathermap.org/data/2.5/forecast?lat=43.7446603&lon=-79.5940434&appid=5e1b862aefc313afac141f2d00567d76`
+  const weather = `https://api.openweathermap.org/data/2.5/forecast?lat=43.7446603&lon=-79.5940434&appid=c9972b4e5fadd10d760467ce0cffc68a`
 
-  const datas = {}
-  const info = []
-  fetch(weather)
-  .then(response => response.json())
-  .then(data => info.push(data.city));
+  const [state, setState] = useState([])
+  const [state2, setState2] = useState({
+    time: [],
+    main: {},
+    weather: {},
+    wind: [],
+  });
+  
+axios(`${weather}`);
 
-  console.log(info)
+useEffect (() => {
+  axios.get(`${weather}`)
+  .then(res => {
+    // console.log('asdas', res.data)
+    // u.push(res.data)
+  //   console.log(Object.values(res.data))
+    setState(res.data.list)
+  //   for (const [key, value] of Object.entries(res.data)) {
+  //     datas[key] = value
+  //     info.push(value)
+  // }
+
+  })    
+}, [])
+
+let test = state
+useEffect (() => {
+  for(let y of test) {
+    // console.log(y.dt)
+    let meep = y.dt
+    let main2 = y.main
+    // setState2({...state, meep});
+    setState2({time: meep});
+  }
+}, [])
+// console.log(state2)
 
   return (
-      <div>    
+      <div>   
+        <p>{state.time} </p>
       </div>
   );
 }
