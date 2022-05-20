32cd449ba7aa1fc0ee08fe21080128984dfd0126
ShayFae <ifrahnoor108@gmail.com>
2022-05-19 21:04:14 -0400

    Test state

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -3,7 +3,7 @@ import './Weather.css'
 import axios from 'axios'
 
 export default function Weather() {
-  const weather = `https://api.openweathermap.org/data/2.5/forecast?lat=43.7446603&lon=-79.5940434&appid=c9972b4e5fadd10d760467ce0cffc68a`
+  const weather = `https://api.openweathermap.org/data/2.5/forecast?lat=43.7446603&lon=-79.5940434&appid=77feb0be968b3068890fc792551fb9d6`
 
   const [state, setState] = useState([])
   const [state2, setState2] = useState({
@@ -37,14 +37,17 @@ useEffect (() => {
     let meep = y.dt
     let main2 = y.main
     // setState2({...state, meep});
-    setState2({time: meep});
+    setState2({time: meep, main: main2});
   }
 }, [])
-// console.log(state2)
+console.log(state2)
+
+let i = state2.main.map(x => x)
+console.log(i)
 
   return (
       <div>   
-        <p>{state.time} </p>
+        <p>Time is: {testTime}</p>
       </div>
   );
 }
